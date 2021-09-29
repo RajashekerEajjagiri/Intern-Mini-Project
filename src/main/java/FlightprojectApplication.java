817ac75c11e6ec8e071d1c41dcc502290baa992f
RajashekerEajjagiri <88499961+RajashekerEajjagiri@users.noreply.github.com>
2021-09-29 23:54:36 +0530

    Add files via upload

---
+++ src/main/java/FlightprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ltts.Flightproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightprojectApplication.class, args);
+	}
+
+}

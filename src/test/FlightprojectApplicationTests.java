61f27b16ed29c8abe5bd8ad1118326da78d0ddc1
RajashekerEajjagiri <88499961+RajashekerEajjagiri@users.noreply.github.com>
2021-09-30 00:20:37 +0530

    Adding Test class

---
+++ src/test/FlightprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltts.Flightproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
